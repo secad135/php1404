2d44fbe4c70d327372e0ad8801250a1bff32e37f
sajjad esmaeili <secad135@gmail.com>
2025-10-03 21:10:25 +0330

    14040711

--- 01_basics/test.php
+++ 01_basics/test.php
@@ -1,25 +1,4 @@
 <?php
-$a = 1.5 ;
-var_dump($a);
+define("ESM" , "meqdar");
 
-echo "<br><br>";
-
-// هم قبل و هم بعد نقطه اعشار فاصله باشد آن را عدد اعشاری فرض نمی کند
-$b = 3 . 9 ;
-var_dump($b);
-
-echo "<br><br>";
-
-$c = -346.890 ;
-var_dump($c);
-
-echo "<br><br>";
-
-$f = 10.00 ;
-var_dump($f);
-
-echo "<br><br>";
-
-// قبل یا بعد از نقطه فاصله باشد خطا میدهد
-// $z = 4 .7 ;
-// var_dump($z);
+echo ESM ;
