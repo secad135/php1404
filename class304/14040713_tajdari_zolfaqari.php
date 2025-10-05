b1c352262a30e16303c76079a6b4648988d73268
sajjad esmaeili <secad135@gmail.com>
2025-10-05 10:33:35 +0330

    14040713

---
+++ class304/14040713_tajdari_zolfaqari.php
@@ -0,0 +1,36 @@
+<?php
+
+echo "سلام محمدامین <br>" ;
+echo "سلام علیرضا " . "<br>";
+
+// توضیحات یک خطی
+echo "جمع مقادیر 5 و 6 برابر است با"/* . 5+6 */ . "تومان<br>";
+
+echo "<br><br>";
+
+$x = 10;
+$y = 20;
+$jame = $x + $y ;
+echo $jame ;
+
+
+$f = "<br><br>"; 
+echo $f;
+
+$x = 10;
+$y = 20;
+$zarb = $x * $y ;
+echo $zarb ;
+
+echo $f ;
+
+$sd = 304;
+echo "من در کلاس $sd درس میخوانم";
+echo $f;
+echo 'من در کلاس $sd درس میخوانم';
+
+echo $f;
+$x = $z = $g = 80;
+echo $x . "<br>";
+echo $z . "<br>";
+echo $g . "<br>";
