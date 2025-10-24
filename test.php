4a87204ae854e97914f0b8ade48686dcda1a9050
secad135 <secad135@gmail.com>
2025-10-24 18:19:22 +0330

    loops update 1

--- test.php
+++ test.php
@@ -1,12 +1,5 @@
 <?php
-// بیست عدد زوج با حلقه فور
 
-$shomarande = 1 ; //شمارنده
-
-for ($adad = 1 ; $shomarande <= 20 ; $adad++){
-    if( $adad % 2 == 0 ){
-        echo $shomarande . " -> " . $adad . "<br>" ;
-        $shomarande++ ;
-    }
-    
+for ($i = 1 ; $i <= 10 ; $i++){
+    echo $i . "<br>" ;
 }
